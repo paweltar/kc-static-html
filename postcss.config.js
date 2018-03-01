9e03613fd39558104843b28d98030b169e2e0d5f
Paweł Targoński <paweltar@gmail.com>
2018-03-01 21:55:52 +0100

    Include uncss to build process to delete unused css classes

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,14 @@
 let tailwindcss = require('tailwindcss');
 let autoprefixer = require('autoprefixer');
+let uncss = require('postcss-uncss');
 
 module.exports = {
   plugins: [
     tailwindcss('./tailwind.js'),
     autoprefixer({ grid: true }),
+    uncss({
+      html: ['*.html'],
+      ignore: []
+    }),
   ]
 }
